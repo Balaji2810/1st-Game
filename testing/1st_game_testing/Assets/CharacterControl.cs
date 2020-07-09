9d7fb115c69ac08901439528da2ee52b5cdbfd09
Balaji2810 <37298992+Balaji2810@users.noreply.github.com>
2020-07-09 10:51:11 +0530

    Character Test - 1

---
+++ testing/1st_game_testing/Assets/CharacterControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public Animator animator;
+
+    private int right, left,speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        left = 0;
+        right = 0;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            left = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            right = 1;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(speed < 3)
+            {
+                speed++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (speed > 0)
+            {
+                speed--;
+            }
+        }
+
+        animator.SetInteger("turn",right+left);
+        animator.SetInteger("speed", speed);
+        
+    }
+}
---
+++ testing/1st_game_testing/Assets/HitReaction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitReaction : MonoBehaviour
+{
+    public int BodyPartNUmber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "obstacle")
+        {
+           
+        }
+        print(collision.gameObject.name);
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+
+        }
+        print(collision.gameObject.name);
+    }
+}
