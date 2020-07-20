bcf9d07faa8e2a9ee4ac83a05ecb5a6c1424ad20
Balaji2810 <37298992+Balaji2810@users.noreply.github.com>
2020-07-20 21:51:09 +0530

    Test -7 Road Generation

---
+++ testing/1st_game_testing/Assets/RoadGen/Script/PathGen.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGen : MonoBehaviour
+{
+    public int RoadLength;
+    public GameObject roadCube;
+    public Material mainColor;
+    public Material sideColor;
+    int[] Bitmap(int n,int point)
+    {
+        int[] bitmap = new int[n];
+        bitmap[point] = 1;
+        if((point-1)>=0)
+        {
+            bitmap[point - 1] = 1;
+        }
+        else
+        {
+            if(Random.Range(0,3)!=0)
+            {
+                bitmap[point + 2] = 1;
+            }
+           
+        }
+        if((point+1) <n)
+        {
+
+            bitmap[point + 1] = 1;
+        }
+        else
+        {
+            if (Random.Range(0, 3) != 0)
+            {
+                bitmap[point - 2] = 1;
+            }
+        }
+       
+       
+        return bitmap;
+    }
+    List<int[]>Path(int length)
+    {
+        List<int[]> roadBitmap = new List<int[]>();
+        int startPosition = Random.Range(0, 5);
+        roadBitmap.Add(Bitmap(5,startPosition));
+        for(int i = 0;i<length-1;i++)
+        {
+            List<int> row = new List<int>();
+
+            for(int j=0;j<roadBitmap[roadBitmap.Count-1].Length;j++)
+            {
+                if(roadBitmap[roadBitmap.Count - 1][j]==1)
+                {
+                    row.Add(j);
+                }
+            }
+            roadBitmap.Add(Bitmap(5, row[Random.Range(0, row.Count)]));
+
+        }
+        
+        return roadBitmap;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      List<int[]> path = Path(RoadLength);
+
+        for (int i = -2; i <= 2; i++) // x - axis
+        {
+            for (int length = 0; length < RoadLength; length++)    // z - axis 
+            {
+                if(path[length][i+2]==1)
+                {
+                    GameObject go = Instantiate(roadCube);
+                    go.transform.localPosition = new Vector3(i, 0, length);
+                    go.transform.parent = transform;
+                    if ((i + length + 2) % 2 == 1)
+                    {
+                        go.GetComponent<MeshRenderer>().material = sideColor;
+                    }
+                }
+                
+            }
+
+        }
+
+    }
+
+   
+}
---
+++ testing/1st_game_testing/Assets/RoadGen/Script/SimpleRoad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRoad : MonoBehaviour
+{
+    public GameObject roadCube;
+    public Material mainColor;
+    public Material sideColor;
+    public int roadLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i=-2;i<=2;i++) // x - axis
+        {
+           for(int length=0;length<roadLength;length++)    // z - axis 
+            {
+                GameObject go = Instantiate(roadCube);
+                go.transform.localPosition = new Vector3(i,0,length);
+                go.transform.parent = transform;
+                if((i+ length +2)%2 == 1)
+                {
+                    go.GetComponent<MeshRenderer>().material = sideColor;
+                }
+            }
+           
+        }
+    }
+
+   
+}
