2c154681e236bcdce60159778c2d892535f808aa
Balaji2810 <37298992+Balaji2810@users.noreply.github.com>
2020-08-20 09:54:06 +0530

    before reboot

---
+++ gaming/New Unity Project/Assets/Script/UIPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPosition : MonoBehaviour
+{
+    public Vector2 position = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Screen.width - Screen.width / position.x;
+        var y = Screen.height - Screen.height / position.y;
+        RectTransform myRectTransform = GetComponent<RectTransform>();
+        myRectTransform.position = new Vector3(x,y, myRectTransform.position.z);
+    }
+}
