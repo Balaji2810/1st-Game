e683a728265c505916e31c638288dfcdfdec6103
Balaji2810 <37298992+Balaji2810@users.noreply.github.com>
2020-10-04 10:24:59 +0530

    Asset Scene

---
+++ gaming/New Unity Project/Assets/Asset Scene/FreeLookController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeLookController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Touch touch;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                print(touch.deltaPosition);
+            }
+        }
+    }
+}
